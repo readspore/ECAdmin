a77705815310c04b3f92f256e27f5cf35978de0d
Dima Lis <readspore@gmail.com>
2020-02-12 01:47:38 +0200

    добавил основные роуты

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECAdmin.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            var controller = RouteData;
+            object slug;
+            var hasSlug = "";
+            if (RouteData.Values.TryGetValue("slug", out slug))
+            {
+                hasSlug = "has slug";
+            }
+            else
+            {
+                hasSlug = "has not slug";
+            }
+            return Content($"product index slug: {hasSlug}");
+        }
+    }
+}
