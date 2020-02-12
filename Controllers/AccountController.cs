b4ec58e2502eeb771de1b9ae0f89f5e2dd663725
Dima Lis <readspore@gmail.com>
2020-02-13 00:18:38 +0200

    login log out views

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -3,9 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using ECAdmin.Models;
-using ECAdmin.ViewModels;
+using ECAdmin.ViewModels.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using ECAdmin.ViewModels.Authorization;
 
 namespace ECAdmin.Controllers
 {
@@ -47,6 +48,48 @@ namespace ECAdmin.Controllers
                 }
             }
             return View(model);
+        }
+        [HttpGet]
+        public IActionResult Login(string returnUrl = null)
+        {
+            return View(new LoginViewModel { ReturnUrl = returnUrl });
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var result =
+                await _signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, false);
+                if (result.Succeeded)
+                {
+                    // проверяем, принадлежит ли URL приложению
+                    if (!string.IsNullOrEmpty(model.ReturnUrl) && Url.IsLocalUrl(model.ReturnUrl))
+                    {
+                        return Redirect(model.ReturnUrl);
+                    }
+                    else
+                    {
+                        return RedirectToAction("Index", "Home");
+                    }
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Неправильный логин и (или) пароль");
+                }
+            }
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Logout()
+        {
+            // удаляем аутентификационные куки
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
         }
     }
 }
--- ViewModels/Authorization/RegisterViewModel.cs
+++ ViewModels/Authorization/RegisterViewModel.cs
@@ -4,7 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace ECAdmin.ViewModels
+namespace ECAdmin.ViewModels.Authorization
 {
     public class RegisterViewModel
     {
