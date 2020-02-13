4b9aef8f62a6a232c3606dbd2f5a745f7651d1ad
Alex <alex.kachmar20@gmail.com>
2020-02-13 22:17:37 +0200

    Added CRUD for post

---
+++ Areas/Admin/Controllers/PostsController.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ECAdmin.Models;
+
+namespace ECAdmin.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class PostsController : Controller
+    {
+        private readonly ApplicationContext _context;
+
+        public PostsController(ApplicationContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Admin/Posts
+        public async Task<IActionResult> Index()
+        {
+            var applicationContext = _context.Posts.Include(p => p.Image);
+            return View(await applicationContext.ToListAsync());
+        }
+
+        // GET: Admin/Posts/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var post = await _context.Posts
+                .Include(p => p.Image)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (post == null)
+            {
+                return NotFound();
+            }
+
+            return View(post);
+        }
+
+        // GET: Admin/Posts/Create
+        public IActionResult Create()
+        {
+            ViewData["ImageId"] = new SelectList(_context.Images, "Id", "Id");
+            return View();
+        }
+
+        // POST: Admin/Posts/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Slug,Name,Type,DateAdded,DateModified,Status,Description,ShortDescription,ImageId")] Post post)
+        {
+            if (ModelState.IsValid)
+            {
+                if (post.DateAdded <= DateTime.MinValue)
+                    post.DateAdded = DateTime.Now;
+                post.DateModified = DateTime.Now;
+                _context.Add(post);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ImageId"] = new SelectList(_context.Images, "Id", "Id", post.ImageId);
+            return View(post);
+        }
+
+        // GET: Admin/Posts/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var post = await _context.Posts.FindAsync(id);
+            if (post == null)
+            {
+                return NotFound();
+            }
+            ViewData["ImageId"] = new SelectList(_context.Images, "Id", "Id", post.ImageId);
+            return View(post);
+        }
+
+        // POST: Admin/Posts/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Slug,Name,Type,DateAdded,DateModified,Status,Description,ShortDescription,ImageId")] Post post)
+        {
+            if (id != post.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    post.DateModified = DateTime.Now;
+                    _context.Update(post);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PostExists(post.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ImageId"] = new SelectList(_context.Images, "Id", "Id", post.ImageId);
+            return View(post);
+        }
+
+        // GET: Admin/Posts/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var post = await _context.Posts
+                .Include(p => p.Image)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (post == null)
+            {
+                return NotFound();
+            }
+
+            return View(post);
+        }
+
+        // POST: Admin/Posts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var post = await _context.Posts.FindAsync(id);
+            _context.Posts.Remove(post);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PostExists(int id)
+        {
+            return _context.Posts.Any(e => e.Id == id);
+        }
+    }
+}
