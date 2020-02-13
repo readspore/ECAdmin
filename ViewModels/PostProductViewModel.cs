bd10cce1a918282f034a2c522709e4bc29ae7797
Alex <alex.kachmar20@gmail.com>
2020-02-13 22:12:34 +0200

    Add CRUD for post

---
+++ ViewModels/PostProductViewModel.cs
@@ -0,0 +1,14 @@
+﻿using ECAdmin.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ECAdmin.Areas.Admin.Models
+{
+    public class PostProductViewModel
+    {
+        public Post Post { get; set; }
+        public Product Product { get; set; }
+    }
+}
