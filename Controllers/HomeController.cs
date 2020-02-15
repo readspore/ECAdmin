69650e7f32463e55c9ccab0d7507cd63aba52ad5
Alex <leksey00022@gmail.com>
2020-02-15 15:05:14 +0200

    Redirect to admin-HomeController if loggined as admin

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -11,7 +11,6 @@ using System.Security.Claims;
 
 namespace ECAdmin.Controllers
 {
-   // [Authorize(Roles = "admin")]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
@@ -26,7 +25,9 @@ namespace ECAdmin.Controllers
 
         public IActionResult Index()
         {
-            
+            //Перенаправление на админовский контроллер если залогинин
+            if (HttpContext.User.IsInRole("admin"))
+                return RedirectToAction("Index", "Home", new { area = "Admin" });
             //var tax1 = new Taxonomy { Name = "Категория 1", PostType = "Post", Slug = "category", Type = "tree" };
             //_context.Taxonomies.AddRange(tax1);
             //var dep1 = new Dependency { Name = "Одежда", Slug = "clothes", Taxonomy = tax1 };
